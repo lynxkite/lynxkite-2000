69ec2352ea5290358b2baf0275a07e95b7cea267
Daniel Darabos <darabos.daniel@gmail.com>
2024-05-17 10:53:26 +0200

    Tweak parameter visuals.

--- web/src/NodeParameter.svelte
+++ web/src/NodeParameter.svelte
@@ -5,48 +5,48 @@
   export let onChange;
 </script>
 
-<div class="param">
-  <label>
-    <span class="param-name">{name.replace('_', ' ')}</span>
-    {#if meta?.type?.collapsed}
-      <button class="collapsed-param form-control form-control-sm">
-        ⋯
-      </button>
-    {:else if meta?.type?.enum}
-      <select class="form-select form-select-sm"
-        value={value}
-        on:change={(evt) => onChange(evt.currentTarget.value)}
-      >
-        {#each meta.type.enum as option}
-          <option value={option}>{option}</option>
-        {/each}
-      </select>
-    {:else}
-      <input class="form-control form-control-sm"
+<label class="param">
+  <span class="param-name">{name.replace(/_/g, ' ')}</span>
+  {#if meta?.type?.collapsed}
+    <button class="collapsed-param form-control form-control-sm">
+      ⋯
+    </button>
+  {:else if meta?.type?.enum}
+    <select class="form-select form-select-sm"
       value={value}
-      on:input={(evt) => onChange(evt.currentTarget.value)}
-      />
-    {/if}
-  </label>
-</div>
+      on:change={(evt) => onChange(evt.currentTarget.value)}
+    >
+      {#each meta.type.enum as option}
+        <option value={option}>{option}</option>
+      {/each}
+    </select>
+  {:else}
+    <input class="form-control form-control-sm"
+    value={value}
+    on:input={(evt) => onChange(evt.currentTarget.value)}
+    />
+  {/if}
+</label>
 
 <style>
   .param {
-    padding: 0 8px 8px 8px;
-  }
-  .param label {
-    font-size: 12px;
+    padding: 4px 8px 4px 8px;
     display: block;
   }
   .param-name {
-    color: #840;
+    display: block;
+    font-size: 10px;
     letter-spacing: 0.05em;
-    margin-left: 4px;
-  }
-  .param input,
-  .param select,
-  .param button {
+    margin-left: 10px;
+    background: oklch(50% 0.13 230);
+    background: oklch(95% 0.2 55);
+    background:var(--bs-border-color);
+    width: fit-content;
+    padding: 2px 8px;
+    border-radius: 4px 4px 0 0;
   }
   .collapsed-param {
+    min-height: 20px;
+    line-height: 10px;
   }
 </style>
