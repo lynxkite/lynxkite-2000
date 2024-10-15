34808b713bfa7868cf08c4bcb928d189ff592b57
Daniel Darabos <darabos.daniel@gmail.com>
2024-10-15 15:24:15 +0200

    Make boxes update on the UI.

--- web/src/NodeWithParams.svelte
+++ web/src/NodeWithParams.svelte
@@ -1,24 +1,23 @@
 <script lang="ts">
   import { getContext } from 'svelte';
-  import { type NodeProps, useSvelteFlow, useUpdateNodeInternals } from '@xyflow/svelte';
+  import { type NodeProps, useNodes } from '@xyflow/svelte';
   import LynxKiteNode from './LynxKiteNode.svelte';
   import NodeParameter from './NodeParameter.svelte';
   type $$Props = NodeProps;
   export let id: $$Props['id'];
   export let data: $$Props['data'];
-  const { updateNodeData } = useSvelteFlow();
-  const updateNodeInternals = useUpdateNodeInternals();
   $: metaParams = data.meta?.params;
   $: store = getContext('LynxKite store');
   function setParam(name, newValue) {
     const i = $store.workspace.nodes.findIndex((n) => n.id === id);
     $store.workspace.nodes[i].data.params[name] = newValue;
-    updateNodeInternals();
   }
-  $: params = data?.params ? Object.entries(data.params) : [];
+  $: params = $nodes && data?.params ? Object.entries(data.params) : [];
+  const nodes = useNodes(); // We don't properly get updates to "data". This is a hack.
+  $: props = $nodes && $$props;
 </script>
 
-<LynxKiteNode {...$$props}>
+<LynxKiteNode {...props}>
   {#each params as [name, value]}
     <NodeParameter
       {name}
--- web/src/NodeWithVisualization.svelte
+++ web/src/NodeWithVisualization.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-  import { type NodeProps } from '@xyflow/svelte';
+  import { useNodes, type NodeProps } from '@xyflow/svelte';
   import NodeWithParams from './NodeWithParams.svelte';
   import { Chart } from 'svelte-echarts';
   import { init } from 'echarts';
   type $$Props = NodeProps;
   export let data: $$Props['data'];
-  $: display = data?.display?.value ? JSON.parse(JSON.stringify(data.display.value)) : {};
+
+  const nodes = useNodes(); // We don't properly get updates to "data". This is a hack.
+  $: D = $nodes && data;
 </script>
 
 <NodeWithParams {...$$props}>
-  {#if data.display}
-    <Chart {init} options={display} initOptions={{renderer: 'canvas', width: 250, height: 250}}/>
-  {/if}
+  <Chart {init} options={D?.display?.value || {}} initOptions={{renderer: 'canvas', width: 250, height: 250}}/>
 </NodeWithParams>
 <style>
 </style>
