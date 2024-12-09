7fda36120d7bffafca3cd657612f90186a55bd60
Daniel Darabos <darabos.daniel@gmail.com>
2024-12-09 12:10:36 +0100

    Missed these files. Workspace in React.

---
+++ web/app/workspace/Workspace.tsx
@@ -0,0 +1,94 @@
+'use client';
+import useSWR from 'swr';
+import { useMemo } from "react";
+import { useSearchParams } from 'next/navigation';
+import {
+  ReactFlow,
+  useNodesState,
+  useEdgesState,
+  Controls,
+  MiniMap,
+  MarkerType,
+  useReactFlow,
+  type XYPosition,
+  type Node,
+  type Edge,
+  type Connection,
+  type NodeTypes,
+} from '@xyflow/react';
+// @ts-ignore
+import ArrowBack from '~icons/tabler/arrow-back.jsx';
+// @ts-ignore
+import Backspace from '~icons/tabler/backspace.jsx';
+// @ts-ignore
+import Atom from '~icons/tabler/atom.jsx';
+import { syncedStore, getYjsDoc } from "@syncedstore/core";
+import { useSyncedStore } from "@syncedstore/react";
+import { WebsocketProvider } from "y-websocket";
+import NodeWithParams from './nodes/NodeWithParams';
+// import NodeWithVisualization from './NodeWithVisualization';
+// import NodeWithImage from './NodeWithImage';
+// import NodeWithTableView from './NodeWithTableView';
+// import NodeWithSubFlow from './NodeWithSubFlow';
+// import NodeWithArea from './NodeWithArea';
+// import NodeSearch from './NodeSearch';
+import EnvironmentSelector from './EnvironmentSelector';
+import { LynxKiteState } from './LynxKiteState';
+import '@xyflow/react/dist/style.css';
+
+export default function Workspace() {
+  const [nodes, setNodes, onNodesChange] = useNodesState([]);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const searchParams = useSearchParams();
+
+  let path = searchParams.get('path');
+  const sstore = syncedStore({ workspace: {} });
+  const doc = getYjsDoc(sstore);
+  const wsProvider = new WebsocketProvider("ws://localhost:8000/ws/crdt", path, doc);
+  const state = useSyncedStore(sstore);
+
+  const fetcher = (resource: string, init?: RequestInit) => fetch(resource, init).then(res => res.json());
+  const catalog = useSWR('/api/catalog', fetcher);
+
+  const nodeTypes = useMemo(() => ({
+    basic: NodeWithParams,
+    table_view: NodeWithParams,
+  }), []);
+  return (
+    <div className="workspace">
+      <div className="top-bar bg-neutral">
+        <a className="logo" href=""><img src="/favicon.ico" /></a>
+        <div className="ws-name">
+          {path}
+        </div>
+        <EnvironmentSelector
+          options={Object.keys(catalog.data || {})}
+          value={state.workspace?.env}
+          onChange={(env) => state.workspace.env = env}
+        />
+        <div className="tools text-secondary">
+          <a href=""><Atom /></a>
+          <a href=""><Backspace /></a>
+          <a href="#dir?path={parentDir}"><ArrowBack /></a>
+        </div>
+      </div>
+      <div style={{ height: "100%", width: '100vw' }}>
+        <LynxKiteState.Provider value={state}>
+          <ReactFlow nodes={state.workspace?.nodes} edges={state.workspace?.edges} nodeTypes={nodeTypes} fitView
+            proOptions={{ hideAttribution: true }}
+            maxZoom={3}
+            minZoom={0.3}
+            defaultEdgeOptions={{ markerEnd: { type: MarkerType.Arrow } }}
+          >
+            <Controls />
+            <MiniMap />
+            {/* {#if nodeSearchSettings}
+          <NodeSearch pos={nodeSearchSettings.pos} boxes={nodeSearchSettings.boxes} on:cancel={closeNodeSearch} on:add={addNode} />
+          {/if} */}
+          </ReactFlow>
+        </LynxKiteState.Provider>
+      </div>
+    </div>
+
+  );
+}
---
+++ web/app/workspace/nodes/LynxKiteNode.tsx
@@ -0,0 +1,90 @@
+import { useContext } from 'react';
+import { LynxKiteState } from '../LynxKiteState';
+import { Handle, NodeResizeControl } from '@xyflow/react';
+// @ts-ignore
+import ChevronDownRight from '~icons/tabler/chevron-down-right.jsx';
+
+interface LynxKiteNodeProps {
+  width: number;
+  height: number;
+  nodeStyle: any;
+  data: any;
+  children: any;
+}
+
+function getHandles(inputs, outputs) {
+  const handles: {
+    position: 'top' | 'bottom' | 'left' | 'right',
+    name: string,
+    index: number,
+    offsetPercentage: number,
+    showLabel: boolean,
+  }[] = [];
+  for (const e of Object.values(inputs)) {
+    handles.push({ ...e, type: 'target' });
+  }
+  for (const e of Object.values(outputs)) {
+    handles.push({ ...e, type: 'source' });
+  }
+  const counts = { top: 0, bottom: 0, left: 0, right: 0 };
+  for (const e of handles) {
+    e.index = counts[e.position];
+    counts[e.position]++;
+  }
+  for (const e of handles) {
+    e.offsetPercentage = 100 * (e.index + 1) / (counts[e.position] + 1);
+    const simpleHorizontal = counts.top === 0 && counts.bottom === 0 && handles.length <= 2;
+    const simpleVertical = counts.left === 0 && counts.right === 0 && handles.length <= 2;
+    e.showLabel = !simpleHorizontal && !simpleVertical;
+  }
+  return handles;
+}
+
+export default function LynxKiteNode(props: LynxKiteNodeProps) {
+  const data = props.data;
+  const state = useContext(LynxKiteState);
+  const expanded = true;
+  const handles = getHandles(data.meta?.inputs || {}, data.meta?.outputs || {});
+  function asPx(n: number | undefined) {
+    return (n ? n + 'px' : undefined) || '200px';
+  }
+  function titleClicked() { }
+  function updateNodeData() { }
+  const handleOffsetDirection = { top: 'left', bottom: 'left', left: 'top', right: 'top' };
+
+  return (
+    <div className={'node-container ' + (expanded ? 'expanded' : 'collapsed')}
+      style={{ width: asPx(props.width), height: asPx(expanded ? props.height : undefined) }}>
+      <div className="lynxkite-node" style={props.nodeStyle}>
+        <div className="title bg-primary" onClick={titleClicked}>
+          {data.title}
+          {data.error && <span className="title-icon">⚠️</span>}
+          {expanded || <span className="title-icon">⋯</span>}
+        </div>
+        {expanded && <>
+          {data.error &&
+            <div className="error">{data.error}</div>
+          }
+          {props.children}
+          {handles.map(handle => (
+            <Handle
+              key={handle.name}
+              id={handle.name} type={handle.type} position={handle.position}
+              style={{ [handleOffsetDirection[handle.position]]: handle.offsetPercentage + '%' }}>
+              {handle.showLabel && <span className="handle-name">{handle.name.replace(/_/g, " ")}</span>}
+            </Handle>
+          ))}
+          <NodeResizeControl
+            minWidth={100}
+            minHeight={50}
+            style={{ 'background': 'transparent', 'border': 'none' }}
+            onResizeStart={() => updateNodeData(id, { beingResized: true })}
+            onResizeEnd={() => updateNodeData(id, { beingResized: false })}
+          >
+            <ChevronDownRight className="node-resizer" />
+          </NodeResizeControl>
+        </>}
+      </div>
+    </div>
+  );
+}
