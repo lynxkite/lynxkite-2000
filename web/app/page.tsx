2fb9e9de8267b191063cb709c8371dee9be57be4
Daniel Darabos <darabos.daniel@gmail.com>
2024-12-09 12:09:37 +0100

    Start converting directory page to React.

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,101 +1,84 @@
+// The directory browser.
 import Image from "next/image";
+import Link from 'next/link';
+// @ts-ignore
+import Home from '~icons/tabler/home'
+// @ts-ignore
+import Folder from '~icons/tabler/folder'
+// @ts-ignore
+import FolderPlus from '~icons/tabler/folder-plus'
+// @ts-ignore
+import File from '~icons/tabler/file'
+// @ts-ignore
+import FilePlus from '~icons/tabler/file-plus'
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+  ;
+  async function fetchList(path) {
+    const encodedPath = encodeURIComponent(path || '');
+    const res = await fetch(`http://localhost:8000/api/dir/list?path=${encodedPath}`);
+    const j = await res.json();
+    return j;
+  }
+  const path = (await searchParams)?.path || '';
+  const list = await fetchList(path);
+  function link(item) {
+    if (item.type === 'directory') {
+      return `/dir?path=${item.name}`;
+    } else {
+      return `/workspace?path=${item.name}`;
+    }
+  }
+  function shortName(item) {
+    return item.name.split('/').pop();
+  }
+  function newName(list) {
+    let i = 0;
+    while (true) {
+      const name = `Untitled${i ? ` ${i}` : ''}`;
+      if (!list.find(item => item.name === name)) {
+        return name;
+      }
+      i++;
+    }
+  }
+  function newWorkspaceIn(path, list) {
+    const pathSlash = path ? `${path}/` : '';
+    return `/workspace?path=${pathSlash}${newName(list)}`;
+  }
+  async function newFolderIn(path, list) {
+    const pathSlash = path ? `${path}/` : '';
+    const name = newName(list);
+    const res = await fetch(`/api/dir/mkdir`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ path: pathSlash + name }),
+    });
+    list = await res.json();
+  }
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="directory-page">
+      <div className="logo">
+        <Link href="https://lynxkite.com/"><img src='/logo.png' className="logo-image" alt="LynxKite logo" /></Link>
+        <div className="tagline">The Complete Graph Data Science Platform</div>
+      </div>
+      <div className="entry-list">
+        <div className="actions">
+          <a href={newWorkspaceIn(path, list)}><FilePlus /> New workspace</a>
+          <a href="" onClick={() => newFolderIn(path, list)}><FolderPlus /> New folder</a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        {path && <div className="breadcrumbs"><a href="#dir"><Home /></a> {path} </div>}
+        {list.map(item =>
+          <a className="entry" href={link(item)}>
+            {item.type === 'directory' ? <Folder /> : <File />}
+            {shortName(item)}
+          </a>
+        )}
+      </div>
     </div>
   );
 }
