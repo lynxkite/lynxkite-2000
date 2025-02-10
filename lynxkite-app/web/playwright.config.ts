aa9111f2f2955eba98d21ae6c25c6aa3094ab3b6
Chema <josemaria.lizano@lynxanalytics.com>
2025-02-10 22:23:09 +0100

    Add playwright tests (#65)
    * Initial setup for Playwright tests
    
    * Install full networkx dependencies
    ---------
    
    Co-authored-by: JMLizano <jmlizlac@gmail.com>

---
+++ lynxkite-app/web/playwright.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig, devices } from '@playwright/test';
+
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 60000,
+  fullyParallel: false,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 1 : 0,
+  workers: 1,
+  reporter: process.env.CI ? [['github'], ['html']] : 'html',
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: 'http://127.0.0.1:8000',
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+    testIdAttribute: 'data-nodeid' // Useful for easily selecting nodes using getByTestId
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+  webServer: {
+    command: 'cd .. && lynxkite',
+    url: 'http://127.0.0.1:8000',
+    reuseExistingServer: !process.env.CI,
+  },
+});
