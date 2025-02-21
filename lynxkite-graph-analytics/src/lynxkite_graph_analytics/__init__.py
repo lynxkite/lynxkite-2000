34bcdc844d10ca7f0773dbcfe74cd9bb17108de1
Daniel Darabos <darabos.daniel@gmail.com>
2025-02-21 18:19:35 +0100

    Graph from molecule similarity.

--- lynxkite-graph-analytics/src/lynxkite_graph_analytics/__init__.py
+++ lynxkite-graph-analytics/src/lynxkite_graph_analytics/__init__.py
@@ -1,3 +1,3 @@
-from . import lynxkite_ops  # noqa (imported to trigger registration)
+from .lynxkite_ops import *  # noqa (imported to trigger registration)
 from . import networkx_ops  # noqa (imported to trigger registration)
 from . import pytorch_model_ops  # noqa (imported to trigger registration)
