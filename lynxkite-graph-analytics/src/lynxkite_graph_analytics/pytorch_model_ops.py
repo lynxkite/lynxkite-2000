edfd36a96354af3dd41a3877004c6dcf65b96372
Daniel Darabos <darabos.daniel@gmail.com>
2025-04-04 10:52:19 +0200

    Split build_model() so we can repeat a part.

--- lynxkite-graph-analytics/src/lynxkite_graph_analytics/pytorch_model_ops.py
+++ lynxkite-graph-analytics/src/lynxkite_graph_analytics/pytorch_model_ops.py
@@ -3,7 +3,6 @@
 import copy
 import enum
 import graphlib
-import types
 
 import pydantic
 from lynxkite.core import ops, workspace
@@ -100,6 +99,11 @@ def activation(x, *, type: ActivationTypes = ActivationTypes.ReLU):
     return Layer(f, shape=x.shape)
 
 
+@op("MSE loss")
+def mse_loss(x, y):
+    return Layer(torch.nn.functional.mse_loss, shape=[1])
+
+
 reg("Softmax", inputs=["x"])
 reg(
     "Graph conv",
@@ -111,7 +115,6 @@ reg("Concatenate", inputs=["a", "b"], outputs=["x"])
 reg("Add", inputs=["a", "b"], outputs=["x"])
 reg("Subtract", inputs=["a", "b"], outputs=["x"])
 reg("Multiply", inputs=["a", "b"], outputs=["x"])
-reg("MSE loss", inputs=["x", "y"], outputs=["loss"])
 reg("Triplet margin loss", inputs=["x", "x_pos", "x_neg"], outputs=["loss"])
 reg("Cross-entropy loss", inputs=["x", "y"], outputs=["loss"])
 reg(
@@ -161,10 +164,10 @@ def _to_id(*strings: str) -> str:
 
 
 @dataclasses.dataclass
-class OpInput:
+class TensorRef:
     """Ops get their inputs like this. They have to return a Layer made for this input."""
 
-    id: str
+    _id: str
     shape: tuple[int, ...]
 
 
@@ -175,11 +178,20 @@ class Layer:
     module: torch.nn.Module
     shapes: list[tuple[int, ...]] | None = None  # One for each output.
     shape: dataclasses.InitVar[tuple[int, ...] | None] = None  # Convenience for single output.
+    # Set by ModelBuilder.
+    _origin_id: str | None = None
+    _inputs: list[TensorRef] | None = None
+    _outputs: list[TensorRef] | None = None
 
     def __post_init__(self, shape):
         assert not self.shapes or not shape, "Cannot set both shapes and shape."
         if shape:
             self.shapes = [shape]
+
+    def _for_sequential(self):
+        inputs = ", ".join(i._id for i in self._inputs)
+        outputs = ", ".join(o._id for o in self._outputs)
+        return self.module, f"{inputs} -> {outputs}"
 
 
 class ColumnSpec(pydantic.BaseModel):
@@ -246,204 +258,212 @@ class ModelConfig:
         }
 
 
-def _add_op(op, params, inputs, outputs, sizes, layers):
-    op_inputs = []
-    for i in op.inputs.keys():
-        id = getattr(inputs, i)
-        op_inputs.append(OpInput(id, shape=sizes.get(id, 1)))
-    if op.func != ops.no_op:
-        layer = op.func(*op_inputs, **params)
-    else:
-        layer = Layer(torch.nn.Identity(), shapes=[i.shape for i in op_inputs])
-    input_ids = ", ".join(i.id for i in op_inputs)
-    output_ids = []
-    for o, shape in zip(op.outputs.keys(), layer.shapes):
-        id = getattr(outputs, o)
-        output_ids.append(id)
-        sizes[id] = shape
-    output_ids = ", ".join(output_ids)
-    layers.append((layer.module, f"{input_ids} -> {output_ids}"))
-
-
-def _all_dependencies(node: str, dependencies: dict[str, list[str]]) -> set[str]:
-    """Returns all dependencies of a node."""
-    deps = set()
-    for dep in dependencies[node]:
-        deps.add(dep)
-        deps.update(_all_dependencies(dep, dependencies))
-    return deps
-
-
 def build_model(ws: workspace.Workspace, inputs: dict[str, torch.Tensor]) -> ModelConfig:
     """Builds the model described in the workspace."""
-    catalog = ops.CATALOGS[ENV]
-    optimizers = []
-    nodes = {}
-    for node in ws.nodes:
-        nodes[node.id] = node
-        if node.data.title == "Optimizer":
-            optimizers.append(node.id)
-    assert optimizers, "No optimizer found."
-    assert len(optimizers) == 1, f"More than one optimizer found: {optimizers}"
-    [optimizer] = optimizers
-    dependencies = {n.id: [] for n in ws.nodes}
-    inv_dependencies = {n.id: [] for n in ws.nodes}
-    in_edges = {}
-    out_edges = {}
-    repeats = []
-    for e in ws.edges:
-        if nodes[e.target].data.title == "Repeat":
-            repeats.append(e.target)
-        dependencies[e.target].append(e.source)
-        inv_dependencies[e.source].append(e.target)
-        in_edges.setdefault(e.target, {}).setdefault(e.targetHandle, []).append(
-            (e.source, e.sourceHandle)
-        )
-        out_edges.setdefault(e.source, {}).setdefault(e.sourceHandle, []).append(
-            (e.target, e.targetHandle)
-        )
-    # Split repeat boxes into start and end, and insert them into the flow.
-    # TODO: Think about recursive repeats.
-    for repeat in repeats:
-        start_id = f"START {repeat}"
-        end_id = f"END {repeat}"
-        # repeat -> first <- real_input
-        # ...becomes...
-        # real_input -> start -> first
-        first, firsth = out_edges[repeat]["output"][0]
-        [(real_input, real_inputh)] = [
-            k for k in in_edges[first][firsth] if k != (repeat, "output")
-        ]
-        dependencies[first].remove(repeat)
-        dependencies[first].append(start_id)
-        dependencies[start_id] = [real_input]
-        out_edges[real_input][real_inputh] = [
-            k if k != (first, firsth) else (start_id, "input")
-            for k in out_edges[real_input][real_inputh]
-        ]
-        in_edges[start_id] = {"input": [(real_input, real_inputh)]}
-        out_edges[start_id] = {"output": [(first, firsth)]}
-        in_edges[first][firsth] = [(start_id, "output")]
-        # repeat <- last -> real_output
-        # ...becomes...
-        # last -> end -> real_output
-        last, lasth = in_edges[repeat]["input"][0]
-        [(real_output, real_outputh)] = [
-            k for k in out_edges[last][lasth] if k != (repeat, "input")
-        ]
-        del dependencies[repeat]
-        dependencies[end_id] = [last]
-        dependencies[real_output].append(end_id)
-        out_edges[last][lasth] = [(end_id, "input")]
-        in_edges[end_id] = {"input": [(last, lasth)]}
-        out_edges[end_id] = {"output": [(real_output, real_outputh)]}
-        in_edges[real_output][real_outputh] = [
-            k if k != (last, lasth) else (end_id, "output")
-            for k in in_edges[real_output][real_outputh]
-        ]
-    # Walk the graph in topological order.
-    sizes = {}
-    for k, i in inputs.items():
-        sizes[k] = i.shape[-1]
-    ts = graphlib.TopologicalSorter(dependencies)
-    layers = []
-    loss_layers = []
-    regions: dict[str, set[str]] = {node_id: set() for node_id in dependencies}
-    cfg = {}
-    used_in_model = set()
-    made_in_model = set()
-    used_in_loss = set()
-    made_in_loss = set()
-    for node_id in ts.static_order():
+    builder = ModelBuilder(ws, inputs)
+    return builder.build_model()
+
+
+class ModelBuilder:
+    """The state shared between methods that are used to build the model."""
+
+    def __init__(self, ws: workspace.Workspace, inputs: dict[str, torch.Tensor]):
+        self.catalog = ops.CATALOGS[ENV]
+        optimizers = []
+        self.nodes = {}
+        for node in ws.nodes:
+            self.nodes[node.id] = node
+            if node.data.title == "Optimizer":
+                optimizers.append(node.id)
+        assert optimizers, "No optimizer found."
+        assert len(optimizers) == 1, f"More than one optimizer found: {optimizers}"
+        [self.optimizer] = optimizers
+        self.dependencies = {n.id: [] for n in ws.nodes}
+        self.in_edges = {}
+        self.out_edges = {}
+        repeats = []
+        for e in ws.edges:
+            if self.nodes[e.target].data.title == "Repeat":
+                repeats.append(e.target)
+            self.dependencies[e.target].append(e.source)
+            self.in_edges.setdefault(e.target, {}).setdefault(e.targetHandle, []).append(
+                (e.source, e.sourceHandle)
+            )
+            self.out_edges.setdefault(e.source, {}).setdefault(e.sourceHandle, []).append(
+                (e.target, e.targetHandle)
+            )
+        # Split repeat boxes into start and end, and insert them into the flow.
+        # TODO: Think about recursive repeats.
+        for repeat in repeats:
+            start_id = f"START {repeat}"
+            end_id = f"END {repeat}"
+            # repeat -> first <- real_input
+            # ...becomes...
+            # real_input -> start -> first
+            first, firsth = self.out_edges[repeat]["output"][0]
+            [(real_input, real_inputh)] = [
+                k for k in self.in_edges[first][firsth] if k != (repeat, "output")
+            ]
+            self.dependencies[first].remove(repeat)
+            self.dependencies[first].append(start_id)
+            self.dependencies[start_id] = [real_input]
+            self.out_edges[real_input][real_inputh] = [
+                k if k != (first, firsth) else (start_id, "input")
+                for k in self.out_edges[real_input][real_inputh]
+            ]
+            self.in_edges[start_id] = {"input": [(real_input, real_inputh)]}
+            self.out_edges[start_id] = {"output": [(first, firsth)]}
+            self.in_edges[first][firsth] = [(start_id, "output")]
+            # repeat <- last -> real_output
+            # ...becomes...
+            # last -> end -> real_output
+            last, lasth = self.in_edges[repeat]["input"][0]
+            [(real_output, real_outputh)] = [
+                k for k in self.out_edges[last][lasth] if k != (repeat, "input")
+            ]
+            del self.dependencies[repeat]
+            self.dependencies[end_id] = [last]
+            self.dependencies[real_output].append(end_id)
+            self.out_edges[last][lasth] = [(end_id, "input")]
+            self.in_edges[end_id] = {"input": [(last, lasth)]}
+            self.out_edges[end_id] = {"output": [(real_output, real_outputh)]}
+            self.in_edges[real_output][real_outputh] = [
+                k if k != (last, lasth) else (end_id, "output")
+                for k in self.in_edges[real_output][real_outputh]
+            ]
+        self.inv_dependencies = {n: [] for n in self.dependencies}
+        for k, v in self.dependencies.items():
+            for i in v:
+                self.inv_dependencies[i].append(k)
+        self.sizes = {}
+        for k, i in inputs.items():
+            self.sizes[k] = i.shape[-1]
+        self.layers = []
+
+    def all_upstream(self, node: str) -> set[str]:
+        """Returns all nodes upstream of a node."""
+        deps = set()
+        for dep in self.dependencies[node]:
+            deps.add(dep)
+            deps.update(self.all_upstream(dep))
+        return deps
+
+    def all_downstream(self, node: str) -> set[str]:
+        """Returns all nodes downstream of a node."""
+        deps = set()
+        for dep in self.inv_dependencies[node]:
+            deps.add(dep)
+            deps.update(self.all_downstream(dep))
+        return deps
+
+    def run_node(self, node_id: str) -> None:
+        """Adds the layer(s) produced by this node to self.layers."""
         if node_id.startswith("START "):
-            node = nodes[node_id.removeprefix("START ")]
+            node = self.nodes[node_id.removeprefix("START ")]
         elif node_id.startswith("END "):
-            node = nodes[node_id.removeprefix("END ")]
+            node = self.nodes[node_id.removeprefix("END ")]
         else:
-            node = nodes[node_id]
+            node = self.nodes[node_id]
         t = node.data.title
-        op = catalog[t]
+        op = self.catalog[t]
         p = op.convert_params(node.data.params)
-        for b in dependencies[node_id]:
-            regions[node_id] |= regions[b]
-        if "loss" in t:
-            regions[node_id].add("loss")
         inputs = {}
-        for n in in_edges.get(node_id, []):
-            for b, h in in_edges[node_id][n]:
+        for n in self.in_edges.get(node_id, []):
+            for b, h in self.in_edges[node_id][n]:
                 i = _to_id(b, h)
                 inputs[n] = i
-                if "loss" in regions[node_id]:
-                    used_in_loss.add(i)
-                else:
-                    used_in_model.add(i)
         outputs = {}
-        for out in out_edges.get(node_id, []):
+        for out in self.out_edges.get(node_id, []):
             i = _to_id(node_id, out)
             outputs[out] = i
-            if not t.startswith("Input:"):  # The outputs of inputs are not "made" by us.
-                if "loss" in regions[node_id]:
-                    made_in_loss.add(i)
-                else:
-                    made_in_model.add(i)
-        inputs = types.SimpleNamespace(**inputs)
-        outputs = types.SimpleNamespace(**outputs)
-        ls = loss_layers if "loss" in regions[node_id] else layers
         match t:
-            case "MSE loss":
-                ls.append(
-                    (
-                        torch.nn.functional.mse_loss,
-                        f"{inputs.x}, {inputs.y} -> {outputs.loss}",
-                    )
-                )
             case "Repeat":
-                ls.append((torch.nn.Identity(), f"{inputs.input} -> {outputs.output}"))
-                sizes[outputs.output] = sizes.get(inputs.input, 1)
-                if node_id.startswith("START "):
-                    regions[node_id].add(("repeat", node_id.removeprefix("START ")))
-                else:
+                if node_id.startswith("END "):
                     repeat_id = node_id.removeprefix("END ")
                     start_id = f"START {repeat_id}"
                     print(f"repeat {repeat_id} ending")
-                    after_start = _all_dependencies(start_id, inv_dependencies)
-                    after_end = _all_dependencies(node_id, inv_dependencies)
-                    before_end = _all_dependencies(node_id, dependencies)
-                    affected_nodes = after_start - after_end
+                    after_start = self.all_downstream(start_id)
+                    after_end = self.all_downstream(node_id)
+                    before_end = self.all_upstream(node_id)
+                    affected_nodes = after_start - after_end - {node_id}
                     repeated_nodes = after_start & before_end
                     assert affected_nodes == repeated_nodes, (
                         f"edges leave repeated section '{repeat_id}':\n{affected_nodes - repeated_nodes}"
                     )
-                    regions[node_id].remove(("repeat", repeat_id))
                     for n in repeated_nodes:
                         print(f"repeating {n}")
             case "Optimizer" | "Input: tensor" | "Input: graph edges" | "Input: sequential":
-                pass
-            case _:
-                _add_op(op, p, inputs, outputs, sizes, ls)
-    cfg["model_inputs"] = list(used_in_model - made_in_model)
-    cfg["model_outputs"] = list(made_in_model & used_in_loss)
-    cfg["loss_inputs"] = list(used_in_loss - made_in_loss)
-    # Make sure the trained output is output from the last model layer.
-    outputs = ", ".join(cfg["model_outputs"])
-    layers.append((torch.nn.Identity(), f"{outputs} -> {outputs}"))
-    # Create model.
-    cfg["model"] = pyg.nn.Sequential(", ".join(cfg["model_inputs"]), layers)
-    # Make sure the loss is output from the last loss layer.
-    [(lossb, lossh)] = in_edges[optimizer]["loss"]
-    lossi = _to_id(lossb, lossh)
-    loss_layers.append((torch.nn.Identity(), f"{lossi} -> loss"))
-    # Create loss function.
-    cfg["loss"] = pyg.nn.Sequential(", ".join(cfg["loss_inputs"]), loss_layers)
-    assert not list(cfg["loss"].parameters()), (
-        f"loss should have no parameters: {list(cfg['loss'].parameters())}"
-    )
-    # Create optimizer.
-    op = catalog["Optimizer"]
-    p = op.convert_params(nodes[optimizer].data.params)
-    o = getattr(torch.optim, p["type"].name)
-    cfg["optimizer"] = o(cfg["model"].parameters(), lr=p["lr"])
-    return ModelConfig(**cfg)
+                return
+        layer = self.run_op(op, p, inputs, outputs)
+        layer._origin_id = node_id
+        self.layers.append(layer)
+
+    def run_op(self, op, params, inputs: dict[str, str], outputs: dict[str, str]) -> Layer:
+        """Returns the layer produced by this op."""
+        op_inputs = [
+            TensorRef(inputs[i], shape=self.sizes.get(inputs[i], 1)) for i in op.inputs.keys()
+        ]
+        if op.func != ops.no_op:
+            layer = op.func(*op_inputs, **params)
+        else:
+            layer = Layer(torch.nn.Identity(), shapes=[i.shape for i in op_inputs])
+        layer._inputs = op_inputs
+        layer._outputs = []
+        for o, shape in zip(op.outputs.keys(), layer.shapes):
+            layer._outputs.append(TensorRef(outputs[o], shape=shape))
+            self.sizes[outputs[o]] = shape
+        return layer
+
+    def build_model(self) -> ModelConfig:
+        # Walk the graph in topological order.
+        ts = graphlib.TopologicalSorter(self.dependencies)
+        for node_id in ts.static_order():
+            self.run_node(node_id)
+        return self.get_config()
+
+    def get_config(self) -> ModelConfig:
+        # Split the design into model and loss.
+        loss_nodes = set()
+        for node_id in self.nodes:
+            if "loss" in self.nodes[node_id].data.title:
+                loss_nodes.add(node_id)
+                loss_nodes |= self.all_downstream(node_id)
+        layers = []
+        loss_layers = []
+        for layer in self.layers:
+            if layer._origin_id in loss_nodes:
+                loss_layers.append(layer)
+            else:
+                layers.append(layer)
+        used_in_model = set(input._id for layer in layers for input in layer._inputs)
+        used_in_loss = set(input._id for layer in loss_layers for input in layer._inputs)
+        made_in_model = set(output._id for layer in layers for output in layer._outputs)
+        made_in_loss = set(output._id for layer in loss_layers for output in layer._outputs)
+        layers = [layer._for_sequential() for layer in layers]
+        loss_layers = [layer._for_sequential() for layer in loss_layers]
+        cfg = {}
+        cfg["model_inputs"] = list(used_in_model - made_in_model)
+        cfg["model_outputs"] = list(made_in_model & used_in_loss)
+        cfg["loss_inputs"] = list(used_in_loss - made_in_loss)
+        # Make sure the trained output is output from the last model layer.
+        outputs = ", ".join(cfg["model_outputs"])
+        layers.append((torch.nn.Identity(), f"{outputs} -> {outputs}"))
+        # Create model.
+        cfg["model"] = pyg.nn.Sequential(", ".join(cfg["model_inputs"]), layers)
+        # Make sure the loss is output from the last loss layer.
+        [(lossb, lossh)] = self.in_edges[self.optimizer]["loss"]
+        lossi = _to_id(lossb, lossh)
+        loss_layers.append((torch.nn.Identity(), f"{lossi} -> loss"))
+        # Create loss function.
+        cfg["loss"] = pyg.nn.Sequential(", ".join(cfg["loss_inputs"]), loss_layers)
+        assert not list(cfg["loss"].parameters()), f"loss should have no parameters: {loss_layers}"
+        # Create optimizer.
+        op = self.catalog["Optimizer"]
+        p = op.convert_params(self.nodes[self.optimizer].data.params)
+        o = getattr(torch.optim, p["type"].name)
+        cfg["optimizer"] = o(cfg["model"].parameters(), lr=p["lr"])
+        print(cfg)
+        return ModelConfig(**cfg)
 
 
 def to_tensors(b: core.Bundle, m: ModelMapping | None) -> dict[str, torch.Tensor]:
