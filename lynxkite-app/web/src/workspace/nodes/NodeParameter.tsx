85b8d8f02ddb4ae1630ad0f755fdf8a12e323b92
Daniel Darabos <darabos.daniel@gmail.com>
2025-04-07 16:59:05 +0200

    Properly show "0" (int) value parameter.

--- lynxkite-app/web/src/workspace/nodes/NodeParameter.tsx
+++ lynxkite-app/web/src/workspace/nodes/NodeParameter.tsx
@@ -25,7 +25,7 @@ function Input({
     <input
       className="input input-bordered w-full"
       ref={inputRef}
-      value={value || ""}
+      value={value ?? ""}
       onChange={(evt) => onChange(evt.currentTarget.value, { delay: 2 })}
       onBlur={(evt) => onChange(evt.currentTarget.value, { delay: 0 })}
       onKeyDown={(evt) => evt.code === "Enter" && onChange(evt.currentTarget.value, { delay: 0 })}
