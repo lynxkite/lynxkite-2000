9072d70829c955112dab7d770f40d7f7d6cab477
Daniel Darabos <darabos.daniel@gmail.com>
2025-04-23 15:57:14 +0200

    Less patient frontend tests.

--- lynxkite-app/web/playwright.config.ts
+++ lynxkite-app/web/playwright.config.ts
@@ -2,19 +2,18 @@ import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./tests",
-  timeout: 30000,
+  timeout: process.env.CI ? 20000 : 10000,
   fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 1 : 0,
-  maxFailures: 5,
+  retries: 0,
+  maxFailures: 3,
   workers: 1,
   reporter: process.env.CI ? [["github"], ["html"]] : "html",
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "http://127.0.0.1:8000",
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: "on",
     testIdAttribute: "data-nodeid", // Useful for easily selecting nodes using getByTestId
   },
   projects: [
@@ -24,7 +23,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: "cd ../../examples && lynxkite",
+    command: "cd ../../examples && LYNXKITE_SUPPRESS_OP_ERRORS=1 lynxkite",
     port: 8000,
     reuseExistingServer: false,
   },
