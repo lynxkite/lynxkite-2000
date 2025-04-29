ceaad39aef88be7a46f17dbedcdcb68f8b4d4a7a
Daniel Darabos <darabos.daniel@gmail.com>
2025-04-29 20:51:12 +0200

    Bigger edge handles. Custom path.

--- lynxkite-app/web/src/workspace/LynxKiteEdge.tsx
+++ lynxkite-app/web/src/workspace/LynxKiteEdge.tsx
@@ -1,22 +1,24 @@
-import { BaseEdge } from "@xyflow/react";
-import { getBetterBezierPath } from "reactflow-better-bezier-edge";
+import { BaseEdge, Position } from "@xyflow/react";
+
+function addOffset(x: number, y: number, p: Position, offset: number) {
+  if (p === Position.Top) return `${x},${y - offset}`;
+  if (p === Position.Bottom) return `${x},${y + offset}`;
+  if (p === Position.Left) return `${x - offset},${y}`;
+  return `${x + offset},${y}`;
+}
 
 export default function LynxKiteEdge(props: any) {
-  const [edgePath] = getBetterBezierPath({
-    sourceX: props.sourceX,
-    sourceY: props.sourceY,
-    sourcePosition: props.sourcePosition,
-    targetX: props.targetX,
-    targetY: props.targetY,
-    targetPosition: props.targetPosition,
-    offset: 0.3 * Math.hypot(props.targetX - props.sourceX, props.targetY - props.sourceY),
-  });
-
+  const offset = 0.3 * Math.hypot(props.targetX - props.sourceX, props.targetY - props.sourceY);
+  const s = addOffset(props.sourceX, props.sourceY, props.sourcePosition, 0);
+  const sc = addOffset(props.sourceX, props.sourceY, props.sourcePosition, offset);
+  const tc = addOffset(props.targetX, props.targetY, props.targetPosition, offset);
+  const t = addOffset(props.targetX, props.targetY, props.targetPosition, 0);
+  const path = `M${s} C${sc} ${tc} ${t}`;
   return (
     <>
       <BaseEdge
         id={props.id}
-        path={edgePath}
+        path={path}
         {...props}
         style={{
           strokeWidth: 2,
