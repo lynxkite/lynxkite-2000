15992618cbc9838dc466b734b9054703f75348d7
Daniel Darabos <darabos.daniel@gmail.com>
2025-06-11 09:07:10 +0200

    Allow sorting tables.

--- lynxkite-app/web/src/workspace/nodes/Table.tsx
+++ lynxkite-app/web/src/workspace/nodes/Table.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function Cell({ value }: { value: any }) {
   if (typeof value === "string") {
     if (value.startsWith("https://") || value.startsWith("data:")) {
@@ -14,17 +16,48 @@ function Cell({ value }: { value: any }) {
 }
 
 export default function Table(props: any) {
+  const [sortColumn, setSortColumn] = useState<string | null>(null);
+  const [sortDirection, setSortDirection] = useState<"asc" | "desc">("asc");
+  function onClick(column: string) {
+    if (sortColumn === column) {
+      setSortDirection(sortDirection === "asc" ? "desc" : "asc");
+    } else {
+      setSortColumn(column);
+      setSortDirection("asc");
+    }
+  }
+  let data = props.data;
+  const sortColumnIndex = props.columns.indexOf(sortColumn);
+  if (sortColumnIndex >= 0) {
+    data = [...props.data];
+    data.sort((a: any[], b: any[]) => {
+      const aValue = a[sortColumnIndex];
+      const bValue = b[sortColumnIndex];
+      if (aValue < bValue) {
+        return sortDirection === "asc" ? -1 : 1;
+      }
+      if (aValue > bValue) {
+        return sortDirection === "asc" ? 1 : -1;
+      }
+      return 0;
+    });
+  }
   return (
     <table className="table-viewer">
       <thead>
         <tr>
           {props.columns.map((column: string) => (
-            <th key={column}>{column}</th>
+            <th key={column} onClick={() => onClick(column)}>
+              {column}
+              {sortColumn === column && (
+                <span className="sort-indicator">{sortDirection === "asc" ? "▲" : "▼"}</span>
+              )}
+            </th>
           ))}
         </tr>
       </thead>
       <tbody>
-        {props.data.map((row: { [column: string]: any }, i: number) => (
+        {data.map((row: { [column: string]: any }, i: number) => (
           <tr key={`row-${i}`}>
             {props.columns.map((_column: string, j: number) => (
               <td key={`cell ${i}, ${j}`}>
