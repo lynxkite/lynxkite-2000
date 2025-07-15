2c9c725477b03942af0ee7c2802d3b89a0c30c98
Gergo Szabo <gergo.szabo@lynxanalytics.com>
2025-07-15 17:49:06 +0200

    Moved Evaluate box into pykeen_ops and updated testing workspace

--- examples/pykeen-testing.py
+++ examples/pykeen-testing.py
@@ -1,81 +1,20 @@
 from lynxkite.core import ops
 from lynxkite_graph_analytics import core
 
-import pandas as pd
 from pykeen import models
-from pykeen import evaluation
 from pykeen.triples import TriplesFactory
-import enum
+
 
 op = ops.op_registration("LynxKite Graph Analytics")
 
 
-class EvaluatorTypes(str, enum.Enum):
-    ClassificationEvaluator = "Classification Evaluator"
-    MacroRankBasedEvaluator = "Macro Rank Based Evaluator"
-    RankBasedEvaluator = "Rank Based Evaluator"
-
-    def to_class(self):
-        return getattr(evaluation, self.name.replace(" ", ""))()
-
-
-@op("Evaluate model")
-def evaluate(
-    bundle: core.Bundle,
-    *,
-    model_name: core.PyKEENModelName = "PyKEENmodel",
-    evaluator: EvaluatorTypes = EvaluatorTypes.RankBasedEvaluator,
-    metrics_str: str = "ALL",
-):
-    """Metrics are a comma separated list, "ALL" if all metrics are needed"""
-    bundle = bundle.copy()
-    model: models.Model = bundle.other.get(model_name)
-    evaluator = evaluator.to_class()
-    testing_triples = TriplesFactory.from_labeled_triples(
-        bundle.dfs["edges_test"][["head", "relation", "tail"]].values
-    )
-    training_triples = TriplesFactory.from_labeled_triples(
-        bundle.dfs["edges_train"][["head", "relation", "tail"]].values
-    )
-    validation_triples = TriplesFactory.from_labeled_triples(
-        bundle.dfs["edges_val"][["head", "relation", "tail"]].values
-    )
-
-    evaluated = evaluator.evaluate(
-        model,
-        testing_triples.mapped_triples,
-        additional_filter_triples=[
-            training_triples.mapped_triples,
-            validation_triples.mapped_triples,
-        ],
-    )
-    if metrics_str == "ALL":
-        bundle.dfs["metrics"] = evaluated.to_df()
-        return bundle
-
-    metrics = metrics_str.split(",")
-    metrics_df = pd.DataFrame(columns=["metric", "score"])
-
-    for metric in metrics:
-        metric = metric.strip()
-        try:
-            score = evaluated.get_metric(metric)
-        except Exception as e:
-            raise Exception(f"Possibly unknown metric: {metric}\nError: {e}")
-        metrics_df = pd.concat(
-            [metrics_df, pd.DataFrame([[metric, score]], columns=metrics_df.columns)]
-        )
-
-    bundle.dfs["metrics"] = metrics_df
-
-    return bundle
-
-
+# TODO: Fix, and update to newest conventions
 @op("Load embedding into PyKEEN model")
 def load_pykeen_embeddings(bundle: core.Bundle, *, _model_name: str):
     """
     Assuming embeddings are torch tensors
     Entity names has to be a file with comma separated entity names
+    Very much work-in-progress...
     """
     node_embeddings = bundle.other["node_embedding"]
     edge_embeddings = bundle.other["edge_embedding"]
--- lynxkite-graph-analytics/src/lynxkite_graph_analytics/pykeen_ops.py
+++ lynxkite-graph-analytics/src/lynxkite_graph_analytics/pykeen_ops.py
@@ -6,6 +6,7 @@ from . import core
 import pandas as pd
 import enum
 from pykeen import models
+from pykeen import evaluation
 from pykeen.pipeline import pipeline
 from pykeen.datasets import get_dataset
 from pykeen.predict import predict_triples, predict_target, predict_all
@@ -441,5 +442,66 @@ def extract_from_pykeen(
         [relation_embeddings[relation_labels.index(label)].numpy() for label in relation_labels]
     )
     bundle.dfs["relations"] = relations_table
+
+    return bundle
+
+
+class EvaluatorTypes(str, enum.Enum):
+    ClassificationEvaluator = "Classification Evaluator"
+    MacroRankBasedEvaluator = "Macro Rank Based Evaluator"
+    RankBasedEvaluator = "Rank Based Evaluator"
+
+    def to_class(self):
+        return getattr(evaluation, self.name.replace(" ", ""))()
+
+
+@op("Evaluate model")
+def evaluate(
+    bundle: core.Bundle,
+    *,
+    model_name: core.PyKEENModelName = "PyKEENmodel",
+    evaluator: EvaluatorTypes = EvaluatorTypes.RankBasedEvaluator,
+    metrics_str: str = "ALL",
+):
+    """Metrics are a comma separated list, "ALL" if all metrics are needed"""
+    bundle = bundle.copy()
+    model: models.Model = bundle.other.get(model_name)
+    evaluator = evaluator.to_class()
+    testing_triples = TriplesFactory.from_labeled_triples(
+        bundle.dfs["edges_test"][["head", "relation", "tail"]].values
+    )
+    training_triples = TriplesFactory.from_labeled_triples(
+        bundle.dfs["edges_train"][["head", "relation", "tail"]].values
+    )
+    validation_triples = TriplesFactory.from_labeled_triples(
+        bundle.dfs["edges_val"][["head", "relation", "tail"]].values
+    )
+
+    evaluated = evaluator.evaluate(
+        model,
+        testing_triples.mapped_triples,
+        additional_filter_triples=[
+            training_triples.mapped_triples,
+            validation_triples.mapped_triples,
+        ],
+    )
+    if metrics_str == "ALL":
+        bundle.dfs["metrics"] = evaluated.to_df()
+        return bundle
+
+    metrics = metrics_str.split(",")
+    metrics_df = pd.DataFrame(columns=["metric", "score"])
+
+    for metric in metrics:
+        metric = metric.strip()
+        try:
+            score = evaluated.get_metric(metric)
+        except Exception as e:
+            raise Exception(f"Possibly unknown metric: {metric}\nError: {e}")
+        metrics_df = pd.concat(
+            [metrics_df, pd.DataFrame([[metric, score]], columns=metrics_df.columns)]
+        )
+
+    bundle.dfs["metrics"] = metrics_df
 
     return bundle
