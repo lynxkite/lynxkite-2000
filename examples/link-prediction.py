0f4047830f394b7ab710d6755fb8f8834f033df1
Gergo Szabo <gergo.szabo@lynxanalytics.com>
2025-07-28 17:31:36 +0200

    Improved demo

--- examples/link-prediction.py
+++ examples/link-prediction.py
@@ -10,7 +10,7 @@ def add_incrementing_id(
     bundle: core.Bundle,
     *,
     table_name: core.TableName,
-    id_column: str = core.ColumnNameByTableName,
+    id_column: core.ColumnNameByTableName,
     save_as: str = "result",
 ):
     """Add an incrementing ID column starting from 0 to a table."""
@@ -18,4 +18,18 @@ def add_incrementing_id(
     df = bundle.dfs[table_name].copy()
     df[id_column] = range(len(df))
     bundle.dfs[save_as] = df
+    return bundle
+
+
+@op("Randomly sample table")
+def random_sample(
+    bundle: core.Bundle,
+    *,
+    table_name: core.TableName,
+    ratio: float,
+    seed: int,
+):
+    bundle = bundle.copy()
+    sampled = bundle.dfs[table_name].sample(frac=ratio, random_state=seed)
+    bundle.dfs[f"{table_name}_sampled"] = sampled
     return bundle
