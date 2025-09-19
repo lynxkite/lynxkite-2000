b742d11d429926c118f815a059561c1b8db05bdf
Daniel Darabos <darabos.daniel@gmail.com>
2025-09-19 15:43:12 +0200

    Table/column binding for outputs.

--- lynxkite-app/web/src/workspace/nodes/ModelMappingParameter.tsx
+++ lynxkite-app/web/src/workspace/nodes/ModelMappingParameter.tsx
@@ -1,16 +1,7 @@
-import { useRef } from "react";
 // @ts-expect-error
 import Close from "~icons/tabler/x.jsx";
 import type { WorkspaceNodeData } from "../../apiTypes";
 import NodeParameter from "./NodeParameter";
-import ParameterInput from "./ParameterInput";
-
-type Bindings = {
-  [key: string]: {
-    df: string;
-    column: string;
-  };
-};
 
 type NamedId = {
   name: string;
@@ -84,23 +75,7 @@ function parseJsonOrEmpty(json: string): object {
   return {};
 }
 
-function getInputDataFrames(data: WorkspaceNodeData): {
-  [df: string]: string[];
-} {
-  const dfs: { [df: string]: string[] } = {};
-  for (const input of getInputs(data)) {
-    if (!input.dataframes) continue;
-    const dataframes = input.dataframes as {
-      [df: string]: { columns: string[] };
-    };
-    for (const [df, { columns }] of Object.entries(dataframes)) {
-      dfs[df] = columns;
-    }
-  }
-  return dfs;
-}
-
-function InputMapping({ meta, params, data, setParam }: any) {
+function BindingMapping({ meta, params, data, setParam }: any) {
   params = { ...params };
   for (const p of meta?.params ?? []) {
     params[p.name] = params[p.name] ?? p.default;
@@ -108,7 +83,7 @@ function InputMapping({ meta, params, data, setParam }: any) {
   return meta.params.map((paramMeta: any) => (
     <NodeParameter
       name={paramMeta.name}
-      key={meta.name + "-" + paramMeta.name}
+      key={paramMeta.name}
       value={params[paramMeta.name]}
       data={{ ...data, params, meta: { value: meta } }}
       meta={paramMeta}
@@ -117,51 +92,35 @@ function InputMapping({ meta, params, data, setParam }: any) {
   ));
 }
 
-function OutputMapping(props: any) {
-  return (
-    <>
-      {/* <select
-				required
-				className="select select-ghost"
-				value={v.map?.[binding.id]?.df}
-				ref={(el) => {
-					dfsRef.current[binding.id] = el;
-				}}
-				onChange={() => onChange(JSON.stringify({ map: getMap() }))}
-			>
-				<option key="" value="" disabled hidden>
-					Choose table
-				</option>
-				{Object.keys(dfs).map((df: string) => (
-					<option key={df} value={df}>
-						{df}
-					</option>
-				))}
-			</select>
-			<ParameterInput
-				inputRef={(el) => {
-					columnsRef.current[binding.id] = el;
-				}}
-				value={v.map?.[binding.id]?.column}
-				onChange={onChange}
-			/>
-			; */}
-    </>
-  );
-}
-
 export default function ModelMapping({ value, onChange, data, variant }: any) {
   const v: any = parseJsonOrEmpty(value);
   v.map ??= {};
   const bindings = getModelBindings(data, variant);
   const handlers = getHandlers(data);
-  function setInputParam(bindingId: string, name: string, newValue: any) {
+  function setBindingParam(bindingId: string, name: string, newValue: any) {
     const newMap = {
       ...v.map,
       [bindingId]: { ...v.map[bindingId], [name]: newValue },
     };
     onChange(JSON.stringify({ map: newMap }));
   }
+  const outputBindingMeta = {
+    params: [
+      {
+        name: "table_name",
+        default: "",
+        type: {
+          format: "dropdown",
+          metadata_query: "[].dataframes[].keys(@)[]",
+        },
+      },
+      {
+        name: "column",
+        default: "",
+        type: "str",
+      },
+    ],
+  };
   return (
     <div className="model-mapping-param">
       <div className="model-mapping-param-tools">
@@ -176,23 +135,14 @@ export default function ModelMapping({ value, onChange, data, variant }: any) {
               <tr key={binding.id}>
                 <td className="model-mapping-param-label">{binding.name}:</td>
                 <td className="model-mapping-param-inputs">
-                  {variant === "output" ? (
-                    <OutputMapping
-                      value={value}
-                      onChange={onChange}
-                      data={data}
-                      variant={variant}
-                    />
-                  ) : (
-                    <InputMapping
-                      data={data}
-                      meta={handlers[binding.id]}
-                      params={v.map[binding.id] ?? {}}
-                      setParam={(name: string, newValue: any) =>
-                        setInputParam(binding.id, name, newValue)
-                      }
-                    />
-                  )}
+                  <BindingMapping
+                    data={data}
+                    meta={variant === "output" ? outputBindingMeta : handlers[binding.id]}
+                    params={v.map[binding.id] ?? {}}
+                    setParam={(name: string, newValue: any) =>
+                      setBindingParam(binding.id, name, newValue)
+                    }
+                  />
                 </td>
               </tr>
             ))
