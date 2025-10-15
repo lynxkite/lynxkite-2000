ba99612abd0275535708a19f9a27d01aae41a205
Daniel Darabos <darabos.daniel@gmail.com>
2025-10-15 22:22:24 +0200

    Styling for Gradio embed.

--- lynxkite-app/web/src/workspace/nodes/NodeWithGradio.tsx
+++ lynxkite-app/web/src/workspace/nodes/NodeWithGradio.tsx
@@ -10,6 +10,8 @@ declare global {
     interface IntrinsicElements {
       "gradio-app": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
         src?: string;
+        theme_mode?: string;
+        container?: string;
       };
     }
   }
@@ -23,15 +25,15 @@ function NodeWithGradio(props: any) {
   const basePath = `${window.location.protocol}//${window.location.host}`;
   const src = `${basePath}${path}/`;
   return (
-    <>
-      <div style={{ margin: "16px" }}>
+    <div style={{ margin: "16px" }}>
+      <div style={{ marginBottom: "16px" }}>
         <a href={src} target="_blank">
           <WindowMaximize style={{ marginRight: "5px" }} />
           Pop out
         </a>
       </div>
-      <gradio-app src={src}></gradio-app>
-    </>
+      <gradio-app src={src} theme_mode="light" container="false"></gradio-app>
+    </div>
   );
 }
 
