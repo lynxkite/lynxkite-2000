76898c641f8e84d299cffee6d5f6ba2f7df50896
Botond Banhidi <banhidiboti@gmail.com>
2026-01-09 11:03:38 +0100

    Added Tooltip to the Envirinment selevtor dropdown

--- lynxkite-app/web/src/workspace/EnvironmentSelector.tsx
+++ lynxkite-app/web/src/workspace/EnvironmentSelector.tsx
@@ -1,20 +1,24 @@
+import Tooltip from "../Tooltip";
+
 export default function EnvironmentSelector(props: {
   options: string[];
   value: string;
   onChange: (val: string) => void;
 }) {
   return (
-    <select
-      className="env-select select w-full max-w-xs"
-      name="workspace-env"
-      value={props.value}
-      onChange={(evt) => props.onChange(evt.currentTarget.value)}
-    >
-      {props.options.map((option) => (
-        <option key={option} value={option}>
-          {option}
-        </option>
-      ))}
-    </select>
+    <Tooltip doc="Select workspace environment">
+      <select
+        className="env-select select w-full max-w-xs"
+        name="workspace-env"
+        value={props.value}
+        onChange={(evt) => props.onChange(evt.currentTarget.value)}
+      >
+        {props.options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </Tooltip>
   );
 }
