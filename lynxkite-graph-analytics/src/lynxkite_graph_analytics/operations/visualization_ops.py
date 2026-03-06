92811f1e56ef0c9ebe2126d06ae32f5a38027f8c
Daniel Darabos <darabos.daniel@gmail.com>
2026-03-06 09:46:18 +0100

    Alphabetize entries in bar charts.

--- lynxkite-graph-analytics/src/lynxkite_graph_analytics/operations/visualization_ops.py
+++ lynxkite-graph-analytics/src/lynxkite_graph_analytics/operations/visualization_ops.py
@@ -160,8 +160,13 @@ def bar_chart(
 ):
     table_x, column_x = x
     table_y, column_y = y
-    dx = b.dfs[table_x][column_x]
-    dy = b.dfs[table_y][column_y]
+    if table_x == table_y:
+        df = b.dfs[table_x]
+    else:
+        df = b.dfs[table_x].merge(b.dfs[table_y], left_index=True, right_index=True)
+    sorted_df = df.sort_values(column_x)
+    dx = sorted_df[column_x]
+    dy = sorted_df[column_y]
     plt.figure(figsize=(6, 6))
     sns.barplot(x=dx, y=dy)
     plt.xlabel(column_x)
